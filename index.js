16bb07a50d5e6972f4cf9173d80034ba78e5aa39
劉承昉 <liuchengfang@liuchengfangdeMacBook-Air.local>
2023-05-14 11:41:41 +0800

    nomode

---
+++ index.js
@@ -0,0 +1,19 @@
+const express=require("express");
+const app =express();
+const mongoose=require("mongoose")
+
+app.set("view engine","ejs");
+app.use(express.jason());
+app.use(express.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/coffee")
+.then(()=>{
+console.log("成功連接coffee資料庫");
+}).catch((e)=>{
+console.log(e);
+}
+
+);
+app.listen(3000,()=>{
+console.log("正在聆聽3000");
+});
